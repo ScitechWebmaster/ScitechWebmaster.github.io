d4b5cccc33f5102d0dd95911b8ddf50555fba3af
Caleb-08 <43527078+Caleb-08@users.noreply.github.com>
2019-02-21 19:40:44 -0600

    Update response.php

--- response.php
+++ response.php
@@ -1,1 +1,13 @@
 
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Response Page</h1>
+<p>Thank you for your response</p> 
+<?php
+echo "Hello World!"; 
+?> 
+<p>we will get right back to you one a provider is available</p>
+</body>
+</html>
